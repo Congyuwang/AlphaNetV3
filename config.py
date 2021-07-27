b2fa1644a4337c1e2ffa9c56eae92f82a8a9d898
Congyuwang <116020237@link.cuhk.edu.cn>
2021-07-27 10:08:52 +0800

    refactor project
    - add config.py
    - add main.py
    - add preprocessing.py

---
+++ config.py
@@ -0,0 +1,36 @@
+# 数据路径
+DATA = "./data/CSI500.zip"
+
+# 时间序列训练参数
+HISTORY_LENGTH = 60
+TRAIN_LENGTH = 1000
+VALIDATE_LENGTH = 150
+SAMPLE_STEP = 2
+
+# 神经网络训练参数
+EPOCHS = 150
+BATCH_SIZE = 500
+DROPOUT = 0.1
+L2 = 0.001
+LEARNING_RATE = 0.0001
+
+# early-stopping
+EARLY_STOPPING_PATIENCE = 100
+# 注意：
+# 当early stopping是False, EARLY_STOPPING_PATIENCE
+
+# 每个rolling period开始的日期
+ROLLING_BEGINNING_LIST = [20110131,
+                          20110731,
+                          20120131,
+                          20120731,
+                          20130131,
+                          20130731,
+                          20140131,
+                          20140731,
+                          20150131,
+                          20150731,
+                          20160131]
+
+# 多次training，降低初始化偶然性的影响
+TRAINING_ID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
--- tests/tests.py
+++ tests/tests.py
@@ -8,64 +8,15 @@ import shutil
 from alphanet import *
 from alphanet.data import *
 from alphanet.metrics import *
+from preprocessing import process_df, build_data_list
 
 
 def prepare_test_data():
     # 测试数据准备
-    csi = pd.read_csv("../data/CSI500.zip", dtype={"代码": "category",
-                                                   "简称": "category"})
-    csi.drop(columns=["简称"], inplace=True)
-
-    # 新增特征
-    csi["close/free_turn"] = csi["收盘价(元)"] / csi["换手率(基准.自由流通股本)"]
-    csi["open/turn"] = csi["开盘价(元)"] / csi["换手率(%)"]
-    csi["volume/low"] = csi["成交量(股)"] / csi["最低价(元)"]
-    csi["vwap/high"] = csi["均价"] / csi["最高价(元)"]
-    csi["low/high"] = csi["最低价(元)"] / csi["最高价(元)"]
-    csi["vwap/close"] = csi["均价"] / csi["收盘价(元)"]
-
-    # 计算t(1)至t(11)回报
-    trading_dates = csi["日期"].unique()
-    trading_dates.sort()
-    dates_shift_dictionary_1 = dict(zip(trading_dates[1:], trading_dates[:-1]))
-    dates_shift_dictionary_11 = dict(zip(trading_dates[11:], trading_dates[:-11]))
-    csi_slice = csi[["代码", "日期", "收盘价(元)"]].copy()
-    csi_slice_date_shift_1 = csi[["代码", "日期", "收盘价(元)"]].copy()
-    csi_slice_date_shift_11 = csi[["代码", "日期", "收盘价(元)"]].copy()
-    csi_slice_date_shift_1["日期"] = csi_slice_date_shift_11["日期"] \
-        .map(lambda x: dates_shift_dictionary_1.get(x, None))
-    csi_slice_date_shift_11["日期"] = csi_slice_date_shift_11["日期"] \
-        .map(lambda x: dates_shift_dictionary_11.get(x, None))
-    csi_slice_date_shift_11.rename(columns={"收盘价(元)": "11交易日后收盘价(元)"},
-                                   inplace=True)
-    csi_slice_date_shift_11.dropna(inplace=True)
-    csi_slice_date_shift_1.rename(columns={"收盘价(元)": "1交易日后收盘价(元)"},
-                                  inplace=True)
-    csi_slice_date_shift_1.dropna(inplace=True)
-    csi_slice = csi_slice.merge(csi_slice_date_shift_1,
-                                how="inner",
-                                left_on=["代码", "日期"],
-                                right_on=["代码", "日期"])
-    csi_slice = csi_slice.merge(csi_slice_date_shift_11,
-                                how="inner",
-                                left_on=["代码", "日期"],
-                                right_on=["代码", "日期"])
-    future_close_price_1 = csi_slice["1交易日后收盘价(元)"]
-    future_close_price_11 = csi_slice["11交易日后收盘价(元)"]
-    csi_slice["10日回报率"] = future_close_price_11 / future_close_price_1 - 1
-    csi_slice.drop(columns=["收盘价(元)", "11交易日后收盘价(元)", "1交易日后收盘价(元)"], inplace=True)
-    csi = csi_slice.merge(csi,
-                          how="inner",
-                          left_on=["代码", "日期"],
-                          right_on=["代码", "日期"])
+    csi = process_df("../data/CSI500.zip")
 
     codes = csi.代码.cat.categories
-    stock_data = []
-    for code in codes:
-        table_part = csi.loc[csi.代码 == code, :]
-        stock_data.append(TimeSeriesData(dates=table_part["日期"].values,
-                                         data=table_part.iloc[:, 3:].values,
-                                         labels=table_part["10日回报率"].values))
+    stock_data = build_data_list(csi)
 
     test_data = tf.constant([stock_data[0].data[0:30],
                              stock_data[0].data[2:32],
