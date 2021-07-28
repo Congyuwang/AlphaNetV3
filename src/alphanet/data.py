719b821bfd5bd3484e1fd37402fa79b8c069d982
Congyuwang <116020237@link.cuhk.edu.cn>
2021-07-28 22:27:18 +0800

    add more type hints and checks

--- src/alphanet/data.py
+++ src/alphanet/data.py
@@ -5,9 +5,9 @@ version: 0.3
 author: Congyu Wang
 date: 2021-07-26
 """
-import tensorflow as tf
-import numpy as np
-from typing import List
+import tensorflow as _tf
+import numpy as _np
+from typing import List as _List
 
 
 __all__ = ["TimeSeriesData", "TrainValData"]
@@ -19,9 +19,9 @@ class TimeSeriesData:
     """
 
     def __init__(self,
-                 dates: np.ndarray,
-                 data: np.ndarray,
-                 labels: np.ndarray):
+                 dates: _np.ndarray,
+                 data: _np.ndarray,
+                 labels: _np.ndarray):
         """
         储存个股的数据信息及预测目标，全部使用numpy，日期格式为整数: `YYYYMMDD`
 
@@ -30,9 +30,9 @@ class TimeSeriesData:
         :param labels: 训练标签Y, 1D numpy array, 长度与dates相同
         """
         # 检查参数类型
-        if (not type(dates) is np.ndarray or
-                not type(data) is np.ndarray or
-                not type(labels) is np.ndarray):
+        if (not type(dates) is _np.ndarray or
+                not type(data) is _np.ndarray or
+                not type(labels) is _np.ndarray):
             raise ValueError("Data should be numpy arrays")
         # 检查日期、数据、标签长度是否一致
         if len(dates) != len(data) or len(dates) != len(labels):
@@ -40,7 +40,7 @@ class TimeSeriesData:
         # 检查维度是否正确
         if dates.ndim != 1 or data.ndim != 2 or labels.ndim != 1:
             raise ValueError("Wrong dimensions")
-        self.dates = dates.astype(np.int32)
+        self.dates = dates.astype(_np.int32)
         self.data = data
         self.labels = labels
 
@@ -51,13 +51,13 @@ class TrainValData:
     """
 
     def __init__(self,
-                 time_series_list: List[TimeSeriesData],
-                 train_length=1200,
-                 validate_length=300,
-                 history_length=30,
-                 train_val_gap=10,
-                 sample_step=2,
-                 fill_na=np.NaN):
+                 time_series_list: _List[TimeSeriesData],
+                 train_length: int = 1200,
+                 validate_length: int = 300,
+                 history_length: int = 30,
+                 train_val_gap: int = 10,
+                 sample_step: int = 2,
+                 fill_na=_np.NaN):
         """
         储存全部的时间序列信息，通过get(start_date)方法获取从start_date
         开始的训练机和验证集。
@@ -99,12 +99,21 @@ class TrainValData:
                 raise ValueError("time_series_data should be a list "
                                  "of TimeSeriesData objects")
         # 检查参数数值
-        if (history_length < 1 or
+        if (type(history_length) is not int or
+                type(validate_length) is not int or
+                type(sample_step) is not int or
+                type(train_length) is not int or
+                type(train_val_gap) is not int or
+                history_length < 1 or
                 validate_length < 1 or
                 sample_step < 1 or
                 train_val_gap < 0 or
                 train_length < history_length):
             raise ValueError("bad arguments")
+
+        if type(fill_na) is not _np.float:
+            raise ValueError("fill_na should be numpy float")
+
         # 确保数据特征数量一致
         self.__feature_counts = time_series_list[0].data.shape[1]
         for series in time_series_list:
@@ -113,24 +122,24 @@ class TrainValData:
                                  "the same number of features")
 
         # 获取日期列表（所有时间序列日期的并集）
-        self.__distinct_dates = np.unique([date for stock in time_series_list
-                                           for date in stock.dates])
+        self.__distinct_dates = _np.unique([date for stock in time_series_list
+                                            for date in stock.dates])
         self.__distinct_dates.sort()
 
         # 聚合数据为(序列(股票), 时间, 特征数量)的张量，缺失数据为np.NaN
         # 标签维度为(序列(股票), 时间)
-        self.__data = np.empty((len(time_series_list),
-                                len(self.__distinct_dates),
-                                self.__feature_counts))
-        self.__labels = np.empty((len(time_series_list),
-                                  len(self.__distinct_dates)))
+        self.__data = _np.empty((len(time_series_list),
+                                 len(self.__distinct_dates),
+                                 self.__feature_counts))
+        self.__labels = _np.empty((len(time_series_list),
+                                   len(self.__distinct_dates)))
         self.__data[:] = fill_na
         self.__labels[:] = fill_na
 
         # 根据日期序列的位置向张量填充数据
         dates_positions = {date: index
                            for index, date in enumerate(self.__distinct_dates)}
-        dates_position_mapper = np.vectorize(lambda d: dates_positions[d])
+        dates_position_mapper = _np.vectorize(lambda d: dates_positions[d])
         for i, series in enumerate(time_series_list):
             # 找到该序列series.dates日期在日期列表中的位置，
             # 将第i个序列填充至tensor的第i行
@@ -139,8 +148,8 @@ class TrainValData:
             self.__labels[i, position_index] = series.labels
 
         # numpy -> tensor
-        self.__data = tf.constant(self.__data, dtype=tf.float32)
-        self.__labels = tf.constant(self.__labels, dtype=tf.float32)
+        self.__data = _tf.constant(self.__data, dtype=_tf.float32)
+        self.__labels = _tf.constant(self.__labels, dtype=_tf.float32)
         self.__train_length = train_length
         self.__validate_length = validate_length
         self.__history_length = history_length
@@ -148,7 +157,7 @@ class TrainValData:
         self.__train_val_gap = train_val_gap
 
     def get(self,
-            start_date,
+            start_date: int,
             order="by_date",
             mode="in_memory"):
         """
@@ -194,8 +203,8 @@ class TrainValData:
         val_x, val_y = __full_tensor_generation__(*val_args)
 
         # 转化为tensorflow DataSet
-        train = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-        val = tf.data.Dataset.from_tensor_slices((val_x, val_y))
+        train = _tf.data.Dataset.from_tensor_slices((train_x, train_y))
+        val = _tf.data.Dataset.from_tensor_slices((val_x, val_y))
         return train, val, dates_info
 
     def __get_from_generator__(self, start_date, order="by_date"):
@@ -206,22 +215,22 @@ class TrainValData:
         train_args, val_args, dates_info = self.__get_period_info__(start_date,
                                                                     order)
         sig = (
-            tf.TensorSpec(
+            _tf.TensorSpec(
                 shape=(self.__history_length,
                        self.__feature_counts),
-                dtype=tf.float32
+                dtype=_tf.float32
             ),
-            tf.TensorSpec(
+            _tf.TensorSpec(
                 shape=(),
-                dtype=tf.float32
+                dtype=_tf.float32
             )
         )
-        train_dataset = tf.data.Dataset.from_generator(__generator__,
-                                                       args=train_args,
-                                                       output_signature=sig)
-        val_dataset = tf.data.Dataset.from_generator(__generator__,
-                                                     args=val_args,
-                                                     output_signature=sig)
+        train_dataset = _tf.data.Dataset.from_generator(__generator__,
+                                                        args=train_args,
+                                                        output_signature=sig)
+        val_dataset = _tf.data.Dataset.from_generator(__generator__,
+                                                      args=val_args,
+                                                      output_signature=sig)
         return train_dataset, val_dataset, dates_info
 
     def __get_period_info__(self, start_date, order="by_date"):
@@ -233,12 +242,12 @@ class TrainValData:
 
         # 找到大于等于start_date的最小日期
         after_start_date = self.__distinct_dates >= start_date
-        first_date = np.min(self.__distinct_dates[after_start_date])
+        first_date = _np.min(self.__distinct_dates[after_start_date])
 
         # 查看剩余日期数量是否大于等于训练集验证集总长度
-        if np.sum(after_start_date) < (self.__train_length +
-                                       self.__validate_length +
-                                       self.__train_val_gap):
+        if _np.sum(after_start_date) < (self.__train_length +
+                                        self.__validate_length +
+                                        self.__train_val_gap):
             raise ValueError("date range exceeded end of dates")
 
         # 计算各个时间节点在时间列表中的位置
@@ -293,7 +302,7 @@ class TrainValData:
                            for series_i in range(len(data))]
 
         if order == "shuffle":
-            np.random.shuffle(generation_list)
+            _np.random.shuffle(generation_list)
         elif order == "by_date":
             pass
         elif order == "by_series":
@@ -302,8 +311,8 @@ class TrainValData:
             raise ValueError("wrong order argument, choose from `shuffle`, "
                              "`by_date`, and `by_series`")
 
-        generation_list = tf.constant(generation_list, dtype=tf.int32)
-        history_length = tf.constant(self.__history_length, dtype=tf.int32)
+        generation_list = _tf.constant(generation_list, dtype=_tf.int32)
+        history_length = _tf.constant(self.__history_length, dtype=_tf.int32)
 
         return data, label, generation_list, history_length
 
@@ -328,17 +337,17 @@ class TrainValData:
         val_time_index += val_start_index
 
         # 训练集在日期列表中的开始位置
-        training_beginning = tf.reduce_min(train_time_index)
+        training_beginning = _tf.reduce_min(train_time_index)
 
         # 结束位置：加上历史长度减去一，获取最大日期位置(inclusive)
-        training_ending = tf.reduce_max(train_time_index)
+        training_ending = _tf.reduce_max(train_time_index)
         training_ending += self.__history_length - 1
 
         # validation集每次取的都是某历史片段末尾的数据
         # 所以加上历史减去一
         validation_index = val_time_index + self.__history_length - 1
-        validation_beginning = tf.reduce_min(validation_index)
-        validation_ending = tf.reduce_max(validation_index)
+        validation_beginning = _tf.reduce_min(validation_index)
+        validation_ending = _tf.reduce_max(validation_index)
 
         dates_info = {
             "training": {
@@ -358,11 +367,11 @@ def __history_expander__(data_tensor, history_length):
     错位叠加历史数据，获取(序列，时间，历史，特征)的四个维度
     """
     total_time_length = data_tensor.shape[1]
-    data_expanded = tf.stack([data_tensor[:, i: (total_time_length + i
-                                                 - history_length + 1), :]
-                              for i in tf.range(history_length)])
+    data_expanded = _tf.stack([data_tensor[:, i: (total_time_length + i
+                                                  - history_length + 1), :]
+                               for i in _tf.range(history_length)])
     # 调整维度顺序 (series, time, history, features)
-    return tf.transpose(data_expanded, perm=[1, 2, 0, 3])
+    return _tf.transpose(data_expanded, perm=[1, 2, 0, 3])
 
 
 def __full_tensor_generation__(data, label, generation_list, history):
@@ -374,14 +383,14 @@ def __full_tensor_generation__(data, label, generation_list, history):
     data_expanded = __history_expander__(data, history)
 
     # 根据generation_list指定的series，日期，获取标签及数据片段
-    label_all = tf.gather_nd(label[:, history - 1:], generation_list)
-    data_all = tf.gather_nd(data_expanded, generation_list)
+    label_all = _tf.gather_nd(label[:, history - 1:], generation_list)
+    data_all = _tf.gather_nd(data_expanded, generation_list)
 
     # 去掉所有包含缺失数据的某股票某时间历史片段
-    label_nan = tf.cast(tf.math.is_nan(label_all), tf.int64)
-    data_nan = tf.cast(tf.math.is_nan(data_all), tf.int64)
-    nan_series_time_index = tf.reduce_sum(tf.reduce_sum(data_nan, axis=2), axis=1)
-    not_nan = tf.add(nan_series_time_index, label_nan) == 0
+    label_nan = _tf.cast(_tf.math.is_nan(label_all), _tf.int64)
+    data_nan = _tf.cast(_tf.math.is_nan(data_all), _tf.int64)
+    nan_series_time_index = _tf.reduce_sum(_tf.reduce_sum(data_nan, axis=2), axis=1)
+    not_nan = _tf.add(nan_series_time_index, label_nan) == 0
     return data_all[not_nan], label_all[not_nan]
 
 
@@ -392,8 +401,8 @@ def __training_example_getter__(data, label, series_i, i, history_length):
     x = data[series_i][i: i + history_length]
     y = label[series_i][i + history_length - 1]
 
-    if (tf.reduce_sum(tf.cast(tf.math.is_nan(x), tf.int64)) == 0 and
-            tf.reduce_sum(tf.cast(tf.math.is_nan(y), tf.int64)) == 0):
+    if (_tf.reduce_sum(_tf.cast(_tf.math.is_nan(x), _tf.int64)) == 0 and
+            _tf.reduce_sum(_tf.cast(_tf.math.is_nan(y), _tf.int64)) == 0):
         return x, y
     else:
         return None
@@ -414,4 +423,4 @@ def __first_index__(array, element):
     """
     计算第一个出现的元素的位置
     """
-    return np.min(np.where(array == element))
+    return _np.min(_np.where(array == element))
--- src/alphanet/metrics.py
+++ src/alphanet/metrics.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
+import tensorflow as _tf
 
 
 __all__ = ["UpDownAccuracy"]
 
 
-class UpDownAccuracy(tf.keras.metrics.Metric):
+class UpDownAccuracy(_tf.keras.metrics.Metric):
     """
     通过对return的预测来计算涨跌准确率
     """
@@ -14,17 +14,17 @@ class UpDownAccuracy(tf.keras.metrics.Metric):
         self.up_down_correct_count = self.add_weight(name='ud_count',
                                                      initializer='zeros',
                                                      shape=(),
-                                                     dtype=tf.float32)
+                                                     dtype=_tf.float32)
         self.length = self.add_weight(name='len',
                                       initializer='zeros',
                                       shape=(),
-                                      dtype=tf.float32)
+                                      dtype=_tf.float32)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_true = tf.cast(y_true > 0.0, tf.float32)
-        y_pred = tf.cast(y_pred > 0.0, tf.float32)
-        length = tf.cast(len(y_true), tf.float32)
-        correct_count = length - tf.reduce_sum(tf.abs(y_true - y_pred))
+        y_true = _tf.cast(y_true > 0.0, _tf.float32)
+        y_pred = _tf.cast(y_pred > 0.0, _tf.float32)
+        length = _tf.cast(len(y_true), _tf.float32)
+        correct_count = length - _tf.reduce_sum(_tf.abs(y_true - y_pred))
 
         self.length.assign_add(length)
         self.up_down_correct_count.assign_add(correct_count)
