d495f8c04bb70b69f004441117bed5a845349713
Congyuwang <52687642+Congyuwang@users.noreply.github.com>
2021-07-29 15:27:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='alphanet',
-    version='0.0.4',
+    version='0.0.5',
     packages=['alphanet'],
     long_description=long_description,
     long_description_content_type="text/markdown",
